103780338fbc0845434225b0675f07e6824b1f1b
Diego-1D <diego1d_fernandes@outlook.com>
2021-07-25 17:02:24 -0300

    Layout dos cards

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Appbar from '../../components/Appbar'
+import AnimalCard from '../../components/AnimalCard';
 
 // import { Container } from './styles';
 
@@ -8,6 +9,7 @@ function Home() {
 
     return <div>
         <Appbar></Appbar>
+        <AnimalCard></AnimalCard>
         <Link to="/profile">perfil</Link>
     </div>;
 }
