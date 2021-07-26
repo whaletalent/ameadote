40b7d0f7f4cde42c716c1de2956857e9e96f7558
Diego-1D <diego1d_fernandes@outlook.com>
2021-07-26 15:45:26 -0300

    Atualização dos cards

--- src/components/AnimalCard/index.js
+++ src/components/AnimalCard/index.js
@@ -1,22 +1,46 @@
 import React from 'react';
-
 import * as S from './styled';
 
 function AnimalCard() {
     return <S.Wrapper>
         <S.Container>
-            <S.Card></S.Card>
-            <S.NomeAnimal>Hugo</S.NomeAnimal>
+            <S.Card>
+                <S.AnimalImage src={'https://o.remove.bg/downloads/cc2dff50-2b6d-45dd-8a7a-58eb091f8cd2/pexels-miguel-constantin-montes-2623968-removebg-preview-removebg-preview.png'} />
+            </S.Card>
+            <S.InforAnimal>
+                <S.AnimalName>Hugo</S.AnimalName>
+                <S.Favorite></S.Favorite>
+            </S.InforAnimal>
+            <S.InforAnimal>
+                <S.AnimalYear>24 meses</S.AnimalYear>
+            </S.InforAnimal>
         </S.Container>
         <S.Container>
-            <S.Card></S.Card>
-            <S.NomeAnimal>Hugo</S.NomeAnimal>
+            <S.Card>
+                <S.AnimalImage src={'https://o.remove.bg/downloads/cc2dff50-2b6d-45dd-8a7a-58eb091f8cd2/pexels-miguel-constantin-montes-2623968-removebg-preview-removebg-preview.png'} />
+            </S.Card>
+            <S.InforAnimal>
+                <S.AnimalName>Hugo</S.AnimalName>
+                <S.Favorite></S.Favorite>
+            </S.InforAnimal>
+            <S.InforAnimal>
+                <S.AnimalYear>24 meses</S.AnimalYear>
+            </S.InforAnimal>
         </S.Container>
         <S.Container>
-            <S.Card></S.Card>
-            <S.NomeAnimal>Hugo</S.NomeAnimal>
+            <S.Card>
+                <S.AnimalImage src={'https://o.remove.bg/downloads/cc2dff50-2b6d-45dd-8a7a-58eb091f8cd2/pexels-miguel-constantin-montes-2623968-removebg-preview-removebg-preview.png'} />
+            </S.Card>
+            <S.InforAnimal>
+                <S.AnimalName>Hugo</S.AnimalName>
+                <S.Favorite></S.Favorite>
+            </S.InforAnimal>
+            <S.InforAnimal>
+                <S.AnimalYear>24 meses</S.AnimalYear>
+            </S.InforAnimal>
         </S.Container>
     </S.Wrapper>;
+    
 }
 
 export default AnimalCard;
